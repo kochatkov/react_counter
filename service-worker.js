952b92652c697705e287c975c0593cb96a19fd7d
kochatkov <vlad.kochatkov@gmail.com>
2020-04-27 16:16:02 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.fbb99dc530400fc9be6f6a55a5783f93.js"
 );
 
 self.addEventListener('message', (event) => {
